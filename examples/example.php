91f74718fad0af83c89dd8b91a81fb1fb4e384a9
Sergiy Chernega <chernegasergiy3@gmail.com>
2024-10-20 00:37:05 +0300

    Update example.php

--- examples/example.php
+++ examples/example.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
-require_once __DIR__ . '/../vendor/autoload.php'; // Include Composer autoload
+require_once __DIR__ . '/../../../autoload.php'; // Include Composer autoload
 
 use ChernegaSergiy\TableMagic\TableMagic;
 
